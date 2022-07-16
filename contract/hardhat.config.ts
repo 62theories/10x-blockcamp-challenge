808f9c8cd4ab55ed20e0f70d442148594021c374
62theories <nattawatx86@gmail.com>
2022-07-16 21:47:13 +0700

    deployed

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
-};
+  solidity: '0.8.9',
+  networks: {
+    bsc_test: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
+}
 
-export default config;
+export default config
--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,23 +1,39 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat'
+import { Bank, ERC20 } from '../typechain-types'
+
+let bank: Bank
+let coin: ERC20
+
+async function deployBankContract(coinAddress: string) {
+  const [owner] = await ethers.getSigners()
+  const Bank = await ethers.getContractFactory('Bank', {
+    signer: owner,
+  })
+  const bank = await Bank.deploy(coinAddress)
+  await bank.deployed()
+  console.log('deployed bank to :', bank.address)
+  return bank
+}
+
+async function deployCoin() {
+  const [owner] = await ethers.getSigners()
+  const Coin = await ethers.getContractFactory('Coin', {
+    signer: owner,
+  })
+  const coin = await Coin.deploy()
+  await coin.deployed()
+  console.log('deployed coin to :', coin.address)
+  return coin
+}
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  coin = await deployCoin()
+  bank = await deployBankContract(coin.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
